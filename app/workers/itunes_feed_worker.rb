1d97da9c252a4ff93dbc85038fd74aca86f0d5ab
remzelg <remycarr@gmail.com>
2019-09-14 23:05:35 -0400

    Move Itunes Categories into config file

--- app/workers/itunes_feed_worker.rb
+++ app/workers/itunes_feed_worker.rb
@@ -1,30 +1,10 @@
 require 'open-uri'
-require 'curb'
 
 class ItunesFeedWorker
   include Sidekiq::Worker
 
   ALPHABET = ('A'..'Z').to_a + ["*"]
-  BASE_URL = "https://itunes.apple.com/us/genre/podcasts-"
-  CATEGORIES =  {
-    # mt = media type. 2 indicates podcasts
-    arts:                         "#{BASE_URL}arts/id1301?mt=2",
-    business:                     "#{BASE_URL}business/id1321?mt=2",
-    comedy:                       "#{BASE_URL}comedy/id1303?mt=2",
-    education:                    "#{BASE_URL}education/id1304?mt=2",
-    games_and_hobbies:            "#{BASE_URL}games-hobbies/id1323?mt=2",
-    government_and_organizations: "#{BASE_URL}government-organizations/id1325?mt=2",
-    health:                       "#{BASE_URL}health/id1307?mt=2",
-    kids_and_family:              "#{BASE_URL}kids-family/id1305?mt=2",
-    music:                        "#{BASE_URL}music/id1310?mt=2",
-    news_and_politics:            "#{BASE_URL}news-politics/id1311?mt=2",
-    religion_and_spirituality:    "#{BASE_URL}religion-spirituality/id1314?mt=2",
-    science_and_medicine:         "#{BASE_URL}science-medicine/id1315?mt=2",
-    society_and_culture:          "#{BASE_URL}society-culture/id1324?mt=2",
-    sports_and_recreation:        "#{BASE_URL}sports-recreation/id1316?mt=2",
-    tv_and_film:                  "#{BASE_URL}tv-film/id1309?mt=2",
-    technology:                   "#{BASE_URL}technology/id1318?mt=2"
-  }
+  CATEGORIES = YAML.load_file(Rails.root.join('config/itunes_categories.yml'))
 
   def perform
     import_categories
@@ -34,6 +14,7 @@ class ItunesFeedWorker
 private
   def import_categories
     CATEGORIES.each do |k, v|
+      # TODO: Call worker with name of category and the url. Run each category import separately
       CSV.open("db/categories/" + k.to_s + ".csv", "wb") do |csv|
         ALPHABET.each do |letter|
           alphabet_page_url = "#{v}&letter=#{letter}"
@@ -54,6 +35,7 @@ private
 
   def find_feeds
     CATEGORIES.each do |k, v|
+      # TODO: Call worker with name of category and the url. Run each category import separately
       CSV.foreach("db/categories/#{k.to_s}.csv") do |row|
         url = row[0]
         # TODO: Remove rescue statement when this is finished testing
